7d5dd8ba0c3ae76c8f575d62275a0ce086494ea0
tliz <tlizzen@gmail.com>
2015-07-22 21:34:49 +0300

    clone repo and modify readme

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  http_basic_authenticate_with name: "mh", password: "secret",
+  http_basic_authenticate_with name: "wt", password: "something",
   except: [:index, :show]
   
   def new
